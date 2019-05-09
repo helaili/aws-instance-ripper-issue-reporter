fdb5b871b6d820908f2997f32863f249458e0f00
Alain Hélaïli <helaili@github.com>
2019-05-09 11:39:53 +0200

    generate the report

--- entrypoint.js
+++ entrypoint.js
@@ -2,5 +2,37 @@ const { Toolkit } = require('actions-toolkit')
 const tools = new Toolkit()
 
 let cache = tools.store.all()
+let regions = Object.getOwnPropertyNames(cache)
+let report = ''
 
-console.log(cache)
+if (regions) {
+  regions.forEach(region => {
+    let regionTable = generateRegionTable(tools.store.get(region))
+    report = report.concat(`### [${region}](https://console.aws.amazon.com/ec2/v2/home?region=${region}#Instances:sort=tag:Name)\n${regionTable}\n`)
+  })
+
+  tools.github.issues.createComment({
+    ...tools.context.issue,
+    body: report
+  })
+} else {
+  tools.github.issues.createComment({
+    ...tools.context.issue,
+    body: 'Nothing to report about our AWS usage'
+  })
+}
+
+function generateRegionTable(reportData) {
+  let markdownTable = null
+
+  if (reportData.length !== 0) {
+    markdownTable = `Instance ID|Name|Owner|Stop|Terminate\n-|-|-|-|-\n`
+  }
+  reportData.forEach(instance => {
+    markdownTable = markdownTable.concat(`${instance.instanceId}|${instance.name}|${instance.owner}|${instance.stop}|${instance.terminate}\n`)
+  })
+  return markdownTable
+}
+
+
+console.log(report)
