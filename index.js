13f76e1d0c5c18a0c074634986dd8ad6c6b7179b
Alain Hélaïli <alain@carbonhub.io>
2020-04-03 23:29:38 +0200

    use core api for logging

--- index.js
+++ index.js
@@ -11,26 +11,24 @@ try {
       let regionTable = generateRegionTable(data.get(region))
       report = report.concat(`### [${region}](https://console.aws.amazon.com/ec2/v2/home?region=${region}#Instances:sort=tag:Name)\n${regionTable}\n`)
     })
-
   } else {
-    
+    core.warning('Nothing to report')
   }
 
-  function generateRegionTable(reportData) {
-    let markdownTable = null
-
-    if (reportData.length !== 0) {
-      markdownTable = `Instance ID|Name|Owner|Stop|Terminate\n-|-|-|-|-\n`
-    }
-    reportData.forEach(instance => {
-      markdownTable = markdownTable.concat(`${instance.instanceId}|${instance.name.value}|${instance.owner.value}|${instance.stop.value}|${instance.terminate.value}\n`)
-    })
-    return markdownTable
-  }
-
-
-  console.log(report)
-
+  core.debug(report)
 } catch (error) {
   core.setFailed(error.message);
+}
+
+
+function generateRegionTable(reportData) {
+  let markdownTable = null
+
+  if (reportData.length !== 0) {
+    markdownTable = `Instance ID|Name|Owner|Stop|Terminate\n-|-|-|-|-\n`
+  }
+  reportData.forEach(instance => {
+    markdownTable = markdownTable.concat(`${instance.instanceId}|${instance.name.value}|${instance.owner.value}|${instance.stop.value}|${instance.terminate.value}\n`)
+  })
+  return markdownTable
 }
